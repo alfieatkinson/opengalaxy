c01ee992e4666b08dd6e1d1ff02786a2d4cc3e25
alfieatkinson <alf.atkinson13@gmail.com>
2025-03-17 15:14:28 +0000

    Refactor code style and formatting across multiple files for consistency

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -2,8 +2,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def send_test_data(request):
-    return Response({
-        "data": "Hello from django backend"
-    })
+    return Response({"data": "Hello from django backend"})
