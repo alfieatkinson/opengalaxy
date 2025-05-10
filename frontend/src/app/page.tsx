7a6fd7dc009aa091652f85f33eb004066753bbb0
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-10 15:23:52 +0100

    Refactor HomePage component to simplify structure and remove unused data fetching logic

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,10 @@
-'use client'
+// src/app/page.tsx
 
-import { useState, useEffect } from 'react'
+import Hero from '@/components/layout/Hero'
 
-export default function HomePage() {
-  const [data, setData] = useState<string | null>(null)
-
-  useEffect(() => {
-    // Fetch data from the backend using a GET request
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/test/`)
-      .then((res) => res.json())
-      .then((data) => setData(data.data))
-      .catch((error) => console.error('Error fetching data:', error))
-  }, []) // Empty dependency array ensures this runs only once on component mount
-
-  return (
-    <div>
-      <h1>Dockerised Full-Stack Template</h1>
-      <h3>With Django, Next.js, Postgres, and Docker</h3>
-      <p>{data ? data : 'Loading data...'}</p> {/* Display data or loading message */}
-    </div>
-  )
+const HomePage = () => {
+  return <Hero />
 }
+
+export default HomePage
+
