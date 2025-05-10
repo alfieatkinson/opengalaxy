751f636c8dcedafdf7d5b220ff8c0e6e802159ac
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-10 16:32:11 +0100

    Fix: Remove unnecessary class from NotFoundPage component

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link'
 
 const NotFoundPage = () => {
   return (
-    <div className="relative flex-grow">
-      {/* absolute inset-0 will span the entire area of the parent <main> */}
+    <div className="flex-grow">
       <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
         <h1 className="text-6xl font-bold mb-4">404</h1>
         <p className="text-xl mb-8">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
