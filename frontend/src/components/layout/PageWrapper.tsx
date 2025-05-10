2155c9112204a3f3e30ba09d6b92f62cc9e234f7
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-10 15:23:41 +0100

    Add PageWrapper component for layout structure with dynamic header and footer

---
+++ frontend/src/components/layout/PageWrapper.tsx
@@ -0,0 +1,36 @@
+// src/components/layout/PageWrapper.tsx
+
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+
+const PageWrapper = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname()
+  const isLandingPage = pathname === '/'
+
+  return (
+    <div 
+      className={`flex flex-col ${
+        isLandingPage ? 'h-screen'
+        : 'min-h-screen items-center'
+      }`}
+    >
+      <Header isLandingPage={isLandingPage} />
+      <main
+        className={[
+          'flex flex-col flex-grow pt-20 pb-4',
+          isLandingPage
+            ? 'items-center justify-center overflow-hidden'
+            : 'max-w-300'
+        ].join(' ')}
+      >
+        {children}
+      </main>
+      <Footer isLandingPage={isLandingPage} />
+    </div>
+  )
+}
+
+export default PageWrapper
