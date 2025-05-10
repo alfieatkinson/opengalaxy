17fc6d2d6651b4554ef1c65a468268dadb498127
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-10 15:23:34 +0100

    Add Hero component for showcasing open-license media

---
+++ frontend/src/components/layout/Hero.tsx
@@ -0,0 +1,18 @@
+// src/components/Hero.tsx
+
+import HighlightedText from '@/components/common/HighlightedText'
+import SearchBar from '@/components/common/SearchBar'
+
+const Hero = () => {
+  return (
+    <div className="w-3/5 flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">
+        Open<HighlightedText>Galaxy</HighlightedText>
+      </h1>
+      <p className="text-lg mb-8">Explore a universe of open-license media, free for all.</p>
+      <SearchBar placeholder="Search for open-license media..." />
+    </div>
+  )
+}
+
+export default Hero
