c853ed1655913481dd7b06cf1ec9be44125780bf
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-10 15:24:02 +0100

    Add NotFoundPage component for handling 404 errors

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,22 @@
+// src/app/not-found.tsx
+
+import Link from 'next/link'
+
+const NotFoundPage = () => {
+  return (
+    <div className="relative flex-grow">
+      {/* absolute inset-0 will span the entire area of the parent <main> */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <p className="text-xl mb-8">
+          Sorry, the page you&apos;re looking for doesn&apos;t exist.
+        </p>
+        <Link href="/" className="btn btn-primary">
+          Go home
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
