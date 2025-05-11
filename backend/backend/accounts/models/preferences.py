f40b69338fe681a3640d1c2595a4d7ecadfc2280
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-11 13:38:05 +0100

    Add UserPreferences model with fields for user preferences

---
+++ backend/backend/accounts/models/preferences.py
@@ -0,0 +1,15 @@
+# backend/backend/accounts/models/preferences.py
+
+from django.db import models
+from django.conf import settings
+
+class UserPreferences(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    public_profile = models.BooleanField(default=True)
+    show_sensitive = models.BooleanField(default=False)
+    blur_sensitive = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "user_preferences"
+        verbose_name = "User Preference"
+        verbose_name_plural = "User Preferences"
