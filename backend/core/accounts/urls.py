5b338a97469e3efa2f1694b881f701abf6850ade
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-12 18:11:35 +0100

    fix: correct typo in registration URL path

--- backend/core/accounts/urls.py
+++ backend/core/accounts/urls.py
@@ -11,7 +11,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("regiser/", RegisterView.as_view(), name="register"),
+    path("register/", RegisterView.as_view(), name="register"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("users/me/", UserDetailView.as_view(), name="user_detail"),
