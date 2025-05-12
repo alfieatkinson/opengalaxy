97b7695fbc08527fc2190cf017484517a34b1dc2
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-12 07:33:33 +0100

    feat: update ESLint and Prettier configurations for improved code quality

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,29 +1,30 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import prettier from 'eslint-plugin-prettier'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierConfig from "./prettier.config.cjs";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-})
+});
 
 export default defineConfig([
-  globalIgnores(['**/node_modules', '**/.next', '**/public', '**/build']),
+  globalIgnores(["**/node_modules", "**/.next", "**/public", "**/build"]),
   {
-    extends: compat.extends('next/typescript', 'plugin:prettier/recommended'), // Had to remove "next/core-web-vitals", as a workaround for an error with eslint-config-next
+    extends: compat.extends("next/typescript", "plugin:prettier/recommended"),
 
     plugins: {
       prettier,
     },
 
     rules: {
-      'prettier/prettier': ['warn'],
+      "prettier/prettier": ["warn", prettierConfig],
     },
   },
-])
+]);
