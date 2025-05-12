fd6f89369a18269323f57edb39bc293dc2520c92
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-12 13:58:28 +0100

    fix: refactor RegisterView to improve password handling and structure

--- backend/core/accounts/views.py
+++ backend/core/accounts/views.py
@@ -3,12 +3,28 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
 
 from .serializer import UserSerializer
 
+User = get_user_model()
+
+# /api/auth/register/
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        # you’ll need the client to supply `password`
+        password = self.request.data.get("password")
+        user = serializer.save()
+        if password:
+            user.set_password(password)
+            user.save()
 
 # /api/auth/token/ and /api/auth/token/refresh/
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -23,7 +39,6 @@ class UserDetailView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
-
 
 # /api/auth/logout/
 class LogoutView(APIView):
