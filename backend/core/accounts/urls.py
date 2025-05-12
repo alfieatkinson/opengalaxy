dd310f7d12880368d79740ccdd2f4c8664a93c40
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-12 13:58:33 +0100

    fix: correct typo in registration URL path

--- backend/core/accounts/urls.py
+++ backend/core/accounts/urls.py
@@ -6,10 +6,12 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     CustomTokenObtainPairView,
     UserDetailView,
+    RegisterView,
     LogoutView,
 )
 
 urlpatterns = [
+    path("regiser/", RegisterView.as_view(), name="register"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("users/me/", UserDetailView.as_view(), name="user_detail"),
