79fe11acedbecc392514815fdfe185a3b2211e12
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-14 12:46:13 +0100

    feat: add user preferences endpoint for retrieving user preferences by username

--- backend/core/accounts/urls.py
+++ backend/core/accounts/urls.py
@@ -9,6 +9,7 @@ from .views import (
     RegisterView,
     LogoutView,
     UserDetailByUsernameView,
+    UserPreferencesView,
 )
 
 urlpatterns = [
@@ -18,5 +19,6 @@ urlpatterns = [
     path("users/me/", UserDetailView.as_view(), name="user_detail"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("users/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_by_username"),
+    path("users/<str:username>/preferences/", UserPreferencesView.as_view(), name="user_preferences"),
     #path("users/<str:username>/favourites/", )
 ]
