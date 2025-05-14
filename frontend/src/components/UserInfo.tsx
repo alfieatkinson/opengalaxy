d04f0ac80c50be26f3b3d6ea8c044e3eb63e1ec0
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-14 10:50:53 +0100

    feat: add UserInfo component for displaying user details

---
+++ frontend/src/components/UserInfo.tsx
@@ -0,0 +1,26 @@
+// src/components/UserInfo.tsx
+
+'use client'
+
+import { User as UserIcon } from 'lucide-react'
+import { User } from '@/lib/profile/types'
+
+interface UserInfoProps {
+  user: User
+}
+
+const UserInfo = ({ user }: UserInfoProps) => {
+  return (
+    <div className="flex absolute items-center space-x-4">
+      <UserIcon size={64} />
+      <div>
+        <h1 className="text-4xl font-bold">{user.username}</h1>
+        <p>
+          {user.first_name} {user.last_name}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default UserInfo
