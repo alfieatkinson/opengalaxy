22b4c32394fd6d5ba403b7be781f77eb662809f2
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-15 17:45:35 +0100

    feat: add href prop to AttributeCard for external linking

--- frontend/src/components/common/AttributeCard.tsx
+++ frontend/src/components/common/AttributeCard.tsx
@@ -6,15 +6,25 @@ interface AttributeCardProps {
   title: string
   icon?: React.ReactNode
   text: string
+  href?: string | null
 }
 
-const AttributeCard = ({ title, icon, text }: AttributeCardProps) => {
+const AttributeCard = ({ title, icon, text, href }: AttributeCardProps) => {
+  if (!href) href = undefined
+
   return (
     <div className="card bg-base-300 shadow-lg w-50">
       <div className="card-body items-center justify-center">
         <h1 className="items-center">{icon}</h1>
-        <h2 className="card-title text-md font-bold">{title}</h2>
-        <p className="text-sm">{text}</p>
+        <a
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`flex flex-col items-center ${href ? 'hover:underline hover:text-primary' : ''}`}
+        >
+          <h2 className="card-title text-md font-bold">{title}</h2>
+          <p className="text-sm">{text}</p>
+        </a>
       </div>
     </div>
   )
