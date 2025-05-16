4445078cb88daf5137ab38a429c34d1bbb1ff86f
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-16 21:10:21 +0100

    feat: integrate BlurOverlay for improved content visibility in FullSizeImage component

--- frontend/src/components/FullSizeImage.tsx
+++ frontend/src/components/FullSizeImage.tsx
@@ -5,6 +5,7 @@
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import type { Media } from '@/lib/media/types'
+import BlurOverlay from '@/components/common/BlurOverlay'
 
 interface FullSizeImageProps {
   media: Media
@@ -20,26 +21,28 @@ const FullSizeImage = ({ media }: FullSizeImageProps) => {
   }, [media.url])
 
   return (
-    <figure className="relative w-full overflow-hidden">
-      <div className="relative w-full aspect-[16/9]">
-        {media.thumbnail_url && (
-          <Image
-            src={media.thumbnail_url}
-            alt={media.title}
-            fill
-            className={`object-contain transition-opacity duration-300 ${
-              loaded ? 'opacity-0' : 'opacity-100'
-            }`}
-          />
-        )}
-        {loaded && (
-          <img
-            src={media.url}
-            alt={media.title}
-            className="absolute top-0 left-0 w-full h-full object-contain"
-          />
-        )}
-      </div>
+    <figure className="relative w-full overflow-hidden aspect-[16/9] rounded-t-lg">
+      <BlurOverlay active={media.mature}>
+        <div className="relative w-full h-full">
+          {media.thumbnail_url && (
+            <Image
+              src={media.thumbnail_url}
+              alt={media.title}
+              fill
+              className={`object-contain transition-opacity duration-300 ${
+                loaded ? 'opacity-0' : 'opacity-100'
+              }`}
+            />
+          )}
+          {loaded && (
+            <img
+              src={media.url}
+              alt={media.title}
+              className="absolute top-0 left-0 w-full h-full object-contain"
+            />
+          )}
+        </div>
+      </BlurOverlay>
     </figure>
   )
 }
