9978a2fe83371588829d732abbc51670338590aa
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-20 18:25:17 +0100

    refactor: adjust layout in SettingsInner component and remove DeleteAccount import

--- frontend/src/components/settings/SettingsInner.tsx
+++ frontend/src/components/settings/SettingsInner.tsx
@@ -4,16 +4,14 @@
 
 import PreferenceSettings from '@/components/settings/PreferenceSettings'
 import AccountSettings from '@/components/settings/AccountSettings'
-import DeleteAccount from '@/components/settings/DeleteAccount'
 
 const SettingsInner = () => {
   return (
-    <div className="w-full max-w-200 p-4 bg-base-200 rounded-lg">
-      <div className="flex flex-row justify-between">
+    <div className="flex flex-col gap-4 w-full max-w-200 p-4 bg-base-200 rounded-lg">
+      <div className="flex flex-row justify-between gap-4">
         <PreferenceSettings />
       </div>
       <AccountSettings />
-      <DeleteAccount />
     </div>
   )
 }
