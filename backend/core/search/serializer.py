c2d45512dc7371247cb2ec1be007569257f615c0
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-21 15:08:23 +0100

    refactor: update fields in SearchHistorySerializer to use 'search_key' and 'search_value' for clarity

--- backend/core/search/serializer.py
+++ backend/core/search/serializer.py
@@ -6,5 +6,5 @@ from .models import SearchHistory
 class SearchHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SearchHistory
-        fields = ["id", "query", "searched_at"]
+        fields = ["id", "search_key", "search_value", "searched_at"]
         read_only_fields = ["id", "searched_at"]
