d21f08d69905e14c0f58310164ad3cf000d767e0
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-21 15:08:00 +0100

    refactor: rename 'query' to 'search_value' and add 'search_key' to SearchHistory model for clarity

--- backend/core/search/models/search_history.py
+++ backend/core/search/models/search_history.py
@@ -6,7 +6,8 @@ from django.conf import settings
 
 class SearchHistory(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    query = models.CharField(max_length=255)
+    search_key = models.CharField(max_length=10, default="q")
+    search_value = models.CharField(max_length=255)
     searched_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
