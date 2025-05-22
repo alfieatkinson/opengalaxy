4cc7d7dd2ac2f0e22b6e0fe2b0b2512529baee69
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-22 10:45:00 +0100

    feat: add support file for Cypress end-to-end tests

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/e2e/support/e2e.ts',
   },
 })
