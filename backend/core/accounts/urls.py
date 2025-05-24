fe1a1bdf0daac3fc5d46c99bbf439d320efda5cd
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-24 22:37:51 +0100

    fix: replace JWT token views with cookie-based token views in URLs

--- backend/core/accounts/urls.py
+++ backend/core/accounts/urls.py
@@ -1,10 +1,10 @@
 # backend/core/accounts/urls.py
 
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import (
+    CookieTokenObtainPairView,
+    CookieTokenRefreshView,
     RegisterView,
     LogoutView,
     UserDetailView,
@@ -16,8 +16,8 @@ from .views import (
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('users/me/', UserDetailView.as_view(), name='user_detail_me'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('users/<str:username>/', UserDetailUpdateDeleteView.as_view(), name='user_detail_update_delete'),
