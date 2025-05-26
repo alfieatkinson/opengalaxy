718d263602c189b3e370ea73c5341c5af1df8d4c
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-26 16:54:48 +0100

    refactor: replace label with div in ThemeSwitcher for improved structure

--- frontend/src/components/shared/ThemeSwitcher.tsx
+++ frontend/src/components/shared/ThemeSwitcher.tsx
@@ -1,24 +1,25 @@
 // src/components/shared/ThemeSwitcher.tsx
 
+import { Sun as SunIcon, Moon as MoonIcon } from 'lucide-react'
 import { useTheme } from '@/context/ThemeContext'
 
 const ThemeSwitcher = () => {
   const [theme, toggleTheme] = useTheme()
 
   return (
-    <label
+    <div
       data-cy="theme-switcher"
-      className="flex items-center justify-between"
+      className="flex min-w-20 items-center justify-between"
       onClick={(e) => e.stopPropagation()}
     >
-      <span className="text-sm mr-4">Theme</span>
+      {theme === 'dark' ? <MoonIcon size={22} /> : <SunIcon size={22} />}
       <input
         type="checkbox"
         className="toggle toggle-primary"
         checked={theme === 'dark'}
         onChange={toggleTheme}
       />
-    </label>
+    </div>
   )
 }
 
