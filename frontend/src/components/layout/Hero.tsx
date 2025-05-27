d82134a9e78946e67211c04988e54813f59a0851
alfieatkinson <alfie@alfieatkinson.dev>
2025-05-27 21:54:57 +0100

    Refactor Hero component layout and add icon for improved visual appeal

--- frontend/src/components/layout/Hero.tsx
+++ frontend/src/components/layout/Hero.tsx
@@ -1,5 +1,6 @@
 // src/components/Hero.tsx
 
+import Image from 'next/image'
 import HighlightedText from '@/components/shared/HighlightedText'
 import SearchBar from '@/components/search/SearchBar'
 import ClientOnly from '@/components/shared/ClientOnly'
@@ -7,10 +8,13 @@ import ClientOnly from '@/components/shared/ClientOnly'
 const Hero = () => {
   return (
     <div className="w-full lg:w-3/5 flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold mb-6 sm:mb-2">
-        Open<HighlightedText>Galaxy</HighlightedText>
-      </h1>
-      <p className="hidden sm:flex text-center text-lg mb-2">
+      <div className="flex items-center mb-2">
+        <h1 className="text-4xl xs:text-5xl font-bold">
+          Open<HighlightedText>Galaxy</HighlightedText>
+        </h1>
+        <Image src="/icon0.svg" alt="Icon" width={80} height={80} />
+      </div>
+      <p className="hidden sm:flex text-center text-lg mb-4">
         Explore a universe of open-licence media, free for all.
       </p>
       <ClientOnly>
