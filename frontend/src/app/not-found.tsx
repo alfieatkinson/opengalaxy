20e5309de7fa5425e9361b2c0ab4c028b2221aae
Alfie Atkinson <50023887+alfieatkinson@users.noreply.github.com>
2025-05-29 10:28:08 +0100

    Fix/searchbar mobile (#59)
    * Refactor layout and search components for improved responsiveness
    
    * Center text in 404 Not Found page for improved readability

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
     <div className="flex-grow">
       <div className="absolute inset-0 flex flex-col items-center justify-center">
         <h1 className="text-6xl font-bold mb-4">404</h1>
-        <p className="text-xl mb-8">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
+        <p className="text-xl mb-8 text-center">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
         <Link href="/" className="btn btn-primary">
           Go home
         </Link>
